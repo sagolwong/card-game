8497526e6aab76fc1e219c12d3ac6324933351d7
sagolwong <5910110678@psu.ac.th>
2018-09-18 23:15:38 +0700

    ประกอบ component และ ส่งผ่าน props ชื่อ value

--- card-game/src/App.js
+++ card-game/src/App.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import CharacterCard from './CharacterCard';
+const word = "Hello";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        Hello {"World"}
-      </div>
+      <div>
+        {
+        Array.from(word).map((c,i) => <CharacterCard value={c} key={i}/>)
+        }      
+        </div>
     );
   }
 }
