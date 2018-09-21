6f3f4f3506f500b28d7ba7134f9e55b4752d25c7
sagolwong <5910110678@psu.ac.th>
2018-09-21 19:11:55 +0700

    ปรับปรุง10.แสดงข้อความต้อนรับเมื่อผู้ใช้ login เข้ามา

--- card-game/src/Footer.js
+++ card-game/src/Footer.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 
 export class Footer extends Component{
+    login(){
+        alert("## Welcome ##")
+    }
     render(){
         return(
             <div>
-                <br></br>Name: <input type="string" onBlur={this.onBlur} />
+                <br></br>Name: <input type="string" onBlur={this.onBlur} /><button onClick={this.login}>login</button>
                 
             </div>
         )
